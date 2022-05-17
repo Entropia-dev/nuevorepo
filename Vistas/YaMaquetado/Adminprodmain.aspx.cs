9a31f181a0dd482dc409e8d345ea6cde938b8be6
Entropia-dev <54455913+Entropia-dev@users.noreply.github.com>
2022-05-17 18:08:31 -0300

    ghost

---
+++ Vistas/YaMaquetado/Adminprodmain.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Vistas.YaMaquetado
+{
+    public partial class AdminPresupuestos : System.Web.UI.Page
+    {
+ 
+    }
+}
